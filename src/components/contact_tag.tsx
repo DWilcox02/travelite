1727a2d6f74d3319974c4100c4343474b75d99a0
Daniel Wilcox <danielgwilcox@gmail.com>
2023-09-16 14:53:32 +0100

    fixed errors. deployed

--- src/components/contact_tag.tsx
+++ src/components/contact_tag.tsx
@@ -10,7 +10,7 @@ function ContactTag({ icon, link }: ContactTagProps) {
 
     return (
         <a className="m-4 p-2 bg-white rounded-md flex justify-center items-center hover:bg-sky-300 transition-all hover:cursor-pointer scale-95  hover:scale-110" href={link}>
-            <div>{createElement(icon, {size: 24})}</div>
+            <div className="text-2xl">{createElement(icon)}</div>
         </a>
     );
 }
