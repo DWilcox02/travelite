3d66efe58204b63d3fe9e7b0f77b0b9fc7e7750c
Daniel Wilcox <danielgwilcox@gmail.com>
2024-09-01 18:27:36 +0200

    Added pet images

--- src/components/pet_sitting_components/pet_desc_large.tsx
+++ src/components/pet_sitting_components/pet_desc_large.tsx
@@ -5,7 +5,11 @@ function PetDescLarge({ service }: SmallDescProps) {
         <div className={`my-4 mx-6 transition-all ${service ? "opacity-100" : "opacity-0"}`}>
             <hr />
             <h1 className=" text-2xl">{service?.description}</h1>
-            <img className="mx-auto max-h-[32rem] w-auto" src={service?.image} alt="" />
+            <div className="flex flex-row overflow-x-scroll gap-4">
+                {service?.examples.map((example, index) => (
+                    <img key={index} className=" rounded-lg mx-auto max-h-[32rem] w-auto" src={example} alt="" />
+                ))}
+            </div>
         </div>
     );
 }
--- src/components/pet_sitting_components/pet_desc_small.tsx
+++ src/components/pet_sitting_components/pet_desc_small.tsx
@@ -5,6 +5,11 @@ function PetDescSmall({ service }: SmallDescProps) {
         <div className={`my-4 mx-6 transition-all ${service ? "opacity-100" : "opacity-0"}`}>
             <hr />
             <h1 className=" text-2xl">{service?.description}</h1>
+            <div className="flex flex-col gap-4">
+                {service?.examples.map((example, index) => (
+                    <img key={index} className=" rounded-lg mx-auto max-h-[32rem] w-auto" src={example} alt="" />
+                ))}
+            </div>
         </div>
     );
 }
